62e64cb691bbc35ab1431016e88d3f336809f44a
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-28 11:08:09 -0800

    [CREATE] Heading component

---
+++ src/Heading/Heading.js
@@ -0,0 +1,15 @@
+
+
+
+
+function Header() {
+    return(
+        <div>
+            <h1>Marty Marinda</h1>
+            <h2>FEW 1.2 Final Assessment</h2>
+        </div>
+        
+    );
+}
+
+export default Header;
