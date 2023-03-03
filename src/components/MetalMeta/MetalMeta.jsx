b048c42631fdf90e1bcb6685674e2d9f1b2a3d58
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-03-02 18:11:56 -0800

    [UPDATE] Changed input font

--- src/components/MetalMeta/MetalMeta.jsx
+++ src/components/MetalMeta/MetalMeta.jsx
@@ -15,12 +15,12 @@ function MetalMeta({ query, setQuery }) {
         <div>
             <p>Total Bands: { totalBands }</p>
             <p>Total Fans: { totalFans }</p>
-            <div className="band-input">
-                <input 
-                    value={query}
-                    placeholder="Browse the Bands!"
-                    onChange={(e) => setQuery(e.target.value)}
-                />
+            <div className="band-input"> 
+            <input 
+                value={query}
+                placeholder="Browse the Bands!"
+                onChange={(e) => setQuery(e.target.value)}
+            />
             </div>
         </div>
     )
